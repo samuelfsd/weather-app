13a8738638c4676d208ddb6e587003d158c4b74b
Samuel Alves <samuel.estudo778@gmail.com>
2022-03-03 11:40:48 -0300

    finishing logic part

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,62 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 import styles from './styles.module.scss';
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7ea8f56b1244666e614be40f5604b2e6`;
+
+  function searchLocation(event: any) {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation('');
+    }
+  }
   return (
     <div className={styles.app}>
+      <div className={styles.search}>
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          placeholder="Enter Location"
+          onKeyPress={searchLocation}
+          type="text"
+        />
+      </div>
       <div className={styles.container}>
         <header className={styles.header}>
           <div className={styles.location}>
-            <p>Dallas</p>
+            <p>{data.name}</p>
           </div>
           <div className={styles.temp}>
-            <h1>65°F</h1>
+            {data.main ? <h1>{data.main.temp}°F</h1> : null}
           </div>
           <div className={styles.description}>
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </header>
-        <section className={styles.content}>
-          <div className={styles.feels}>
-            <p className={styles.bold}>65°F</p>
-            <p>Feels Like</p>
-          </div>
-          <div className={styles.humidity}>
-            <p className={styles.bold}>20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className={styles.wind}>
-            <p className={styles.bold}>12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </section>
+        {data.name != undefined && (
+          <section className={styles.content}>
+            <div className={styles.feels}>
+              {data.main ? <p className={styles.bold}>{data.main.feels_like}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className={styles.humidity}>
+              {data.main ? <p className={styles.bold}>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className={styles.wind}>
+              {data.wind ? <p className={styles.bold}>{data.wind.speed}MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </section>
+        )}
       </div>
     </div>
   );
--- src/services/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=7ea8f56b1244666e614be40f5604b2e6`,
-});
